a077e6b0b5061d412502294c7085759375053a52
Mia-babson2025 <mvittozzi1@babson.edu>
2025-09-13 20:55:48 +0000

    Main

---
+++ labs/lab1/LAB 1 Mia V.py
@@ -0,0 +1,23 @@
+def main():
+    while True:
+        print("Lab 1 - Python Basics")
+        print("1. Draw Diamond")
+        print("2. Text Analysis")
+        print("3. Caesar Cipher")
+        choice = input("Select part to run (1-3): ")
+        
+        if choice == "1":
+            draw_diamond()
+        elif choice == "2":
+            text_analysis()
+        elif choice == "3":
+            caesar_cipher()
+        else:
+            exit()
+main()
+
+def draw_diamond():
+    
+def text_analysis():
+    
+def caesar_cipher():
