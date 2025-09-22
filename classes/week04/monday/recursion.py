3c551e013acd5551815710ce8e5a8c4b47ae73a9
Mia-babson2025 <mvittozzi1@babson.edu>
2025-09-22 15:15:41 +0000

    update recursion.py and synatic_candy.ipynb

--- classes/week04/monday/recursion.py
+++ classes/week04/monday/recursion.py
@@ -1,8 +1,18 @@
 def myPrint(n):
     
-    print("hello", n)       
+    print("hello", n)  
+    if n == 5: return 'done'  #stopping statement    
     myPrint(n + 1)
+print(myPrint(1))
 
+
+def recur_sum(n):
+    if n==0:
+        return 0
+    else:
+        return n + recur_sum(n-1)
+
+print(recur_sum(5))
     
     
 
